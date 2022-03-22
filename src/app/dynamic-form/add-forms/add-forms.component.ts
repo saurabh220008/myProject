a85653556d8d882d48b1cf8e37dc32946e92edd4
saurabh220008 <ty38saurabh@gmail.com>
2022-03-22 18:35:13 +0530

    third coomit

--- src/app/dynamic-form/add-forms/add-forms.component.ts
+++ src/app/dynamic-form/add-forms/add-forms.component.ts
@@ -10,28 +10,61 @@ import { table } from '../table.model';
 export class AddFormsComponent implements OnInit {
   addForm: any;
   NOV: any;
+  formsubmit: boolean;
 
   constructor(private fb: FormBuilder) { }
   dynamicArray: Array<table> = [];
+  customer: any;
   newDynamic: any = {};
 
   ngOnInit(): void {
-
-    // this.newDynamic = { vName: "", vNumber: "", vModel: "", NOW: "" };
-    // this.dynamicArray.push(this.newDynamic);
 
     this.addForm = this.fb.group({
       name: ["", Validators.required],
       lastname: ["", Validators.required],
       dob: ["", Validators.required],
       vehicle: ["", Validators.required],
-      contact: ["", Validators.required]
+      contact: ["", Validators.required],
+      vName: ["", Validators.required],
+      vNumber: ["", Validators.required],
+      vModel: ["", Validators.required],
+      NOW: ["", Validators.required]
     })
 
   }
 
   onSubmit(form: FormGroup) {
+    debugger;
+    this.formsubmit = true;
+    if (this.addForm.valid) {
+      console.log(JSON.stringify(form.value));
+      var customers:[] =[];
+      // customer
+      // const customerJSON = localStorage.getItem('customer');
+      // const customer: any[] = customerJSON ? JSON.parse(customerJSON) : [];
+      // customer.push(this.addForm.get("name").value);
+      // customer.push(this.addForm.get("lastname").value);
+      // customer.push(this.addForm.get("dob").value);
+      // customer.push(this.addForm.get("vehicle").value);
+      // customer.push(this.addForm.get("contact").value);
+      // this.Vehicle = this.dynamicArray;
+      // customer.push(this.Vehicle);
+      // customers.push(customer)
+      // console.log(customer);
+      // const data1 = JSON.parse(JSON.stringify(customerJSON));
 
+      this.customer={
+        "name":this.addForm.get("name").value,
+        "lastname":this.addForm.get("lastname").value,
+        "dob":this.addForm.get("dob").value,
+        "vehicle":this.dynamicArray,
+        "contact":this.addForm.get("contact").value
+      }
+
+      console.log(this.customer);
+      localStorage.setItem("customer", JSON.stringify(this.customer));
+
+    }
   }
 
   onKeyDownEvent(event: any) {
@@ -43,14 +76,29 @@ export class AddFormsComponent implements OnInit {
 
   addRow(n: number) {
     debugger;
-    if (n == this.NOV) {
-      for (n = 1; n >= n; n--) {
+    if (n <= this.NOV) {
+      while (n != 0) {
+        // for (n=0; n < this.NOV; n++) {
         this.newDynamic = { vName: "", vNumber: "", vModel: "", NOW: "" };
         this.dynamicArray.push(this.newDynamic);
         console.log(this.dynamicArray);
+        // }
+        n--;
+        this.NOV--;
       }
+      this.NOV--;
     }
-    return true;
+    // return true;
+  }
+
+  deleteRow(n: number) {
+    debugger;
+    if (this.dynamicArray.length == 1) {
+      return false;
+    } else {
+      this.dynamicArray.splice(n, 1);
+      return true;
+    }
   }
 
 }
--- src/app/user/sign-in/sign-in.component.ts
+++ src/app/user/sign-in/sign-in.component.ts
@@ -12,19 +12,35 @@ export class SignInComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   signInForm: any;
-  alphaNumeric="/^[a-zA-Z0-9]+$/";
+  alphaNumeric = "/^[a-zA-Z0-9]+$/";
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
-  contactPattern ="^((\\+91-?)|0)?[0-9]{10}$";
+  // contactPattern = "^((\\+91-?)|0)?[0-9]{10}$";
+  male: any;
+  female: any;
+  other: any;
 
   ngOnInit(): void {
     this.signInForm = this.fb.group({
-      name: ['', [Validators.required, Validators.pattern(this.alphaNumeric)]],
-      email: ['', [ Validators.required, Validators.pattern(this.emailPattern)]],
-      contact: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"), Validators.maxLength(10), Validators.minLength(10)]],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+      contact: ['', [Validators.required, Validators.pattern('[0-9]*'), Validators.maxLength(10), Validators.minLength(10)]],
       gender: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      Mname: ['', [Validators.required]],
+      Fname: ['', [Validators.required]],
+      Other: ['', [Validators.required]]
     });
-   
+
+    this.male = {
+      "display": "none"
+    };
+    this.female = {
+      "display": "none"
+    };
+    this.other = {
+      "display": "none"
+    };
+
     console.log(this.signInForm.controls)
   }
   // private formsubmit : boolean;
@@ -32,14 +48,64 @@ export class SignInComponent implements OnInit {
 
     debugger;
     this.formsubmit = true;
-    if(this.signInForm.valid){
+    if (this.signInForm.valid) {
       console.log(JSON.stringify(form.value))
     }
   }
 
-  get f()
-  {
-  
+  get f() {
+
     return this.signInForm.controls;
   }
+
+  selectgen(event: any) {
+    debugger
+    var x: boolean = false;
+    if (event.target.value == "Male") {
+      this.male = {
+        "display": "inline"
+      };
+      this.female = {
+        "display": "none"
+      };
+      this.other = {
+        "display": "none"
+      };
+    }
+    else if (event.target.value == "Female") {
+      this.female = {
+        "display": "inline"
+      };
+      this.male = {
+        "display": "none"
+      };
+      this.other = {
+        "display": "none"
+      };
+    }
+    else if (event.target.value == "Other") {
+      this.other = {
+        "display": "inline"
+      };
+      this.female = {
+        "display": "none"
+      };
+      this.male = {
+        "display": "none"
+      };
+    }
+  }
+
+  // _keyUp(event: any) {
+  //   debugger
+  //   const pattern = /[0-9\+\-\ ]/;
+  //   let inputChar = String.fromCharCode(event.key);
+
+  //   if (!pattern.test(inputChar)) {
+  //     // invalid character, prevent input
+  //     event.preventDefault();
+  //   }
+  // }
+
+
 }
