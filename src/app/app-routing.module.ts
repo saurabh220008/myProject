8d818a97fbfd3b9d29fb675debd4dbe3c4819eaa
saurabh220008 <ty38saurabh@gmail.com>
2022-03-31 22:14:36 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,33 +4,34 @@ import { LoginAuthGuard } from './Authgurd/login-auth.guard';
 import { StepLogInComponent } from './MyComponents/step-log-in/step-log-in.component';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: '/stepform',
+  //   pathMatch: 'full',
+  // },
   {
-    path: '',
-    redirectTo: '/stepform',
-    pathMatch: 'full'
-  },
-  {
-    path: "stepform",
+    path: 'stepform',
     component: StepLogInComponent,
-    canActivate:[LoginAuthGuard]
+    // canActivate:[LoginAuthGuard]
   },
   {
-    path: "user",
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
   },
   {
-    path: "curd",
-    loadChildren: () => import('./curd/curd.module').then(m => m.CurdModule),
-    canActivate:[LoginAuthGuard]
+    path: 'curd',
+    loadChildren: () => import('./curd/curd.module').then((m) => m.CurdModule),
+    canActivate: [LoginAuthGuard],
   },
   {
-    path: "account",
-    loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
-  }
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((m) => m.AccountModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
